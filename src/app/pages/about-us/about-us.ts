a986c90b526e2e14e54b27913fcf2873bf4ce8e6
alirezaSoleymani <a83.soleimani@gmail.com>
2026-05-06 02:51:26 +0330

    WIP: add basic slider using swiper library, still not customized and polished

--- src/app/pages/about-us/about-us.ts
+++ src/app/pages/about-us/about-us.ts
@@ -1,17 +1,34 @@
 import { ContentService } from '../../shared/services/content/content-service';
 import { Navbar } from './../../shared/components/navbar/navbar';
-import { Component, inject, signal } from '@angular/core';
+import {
+  Component,
+  inject,
+  signal,
+  CUSTOM_ELEMENTS_SCHEMA,
+} from '@angular/core';
+// import { SwiperComponent, SwiperSlideDirective } from 'swiper/angular';
+// import { Navigation } from 'swiper/modules';
+// import { register } from 'swiper/element/bundle';
+import 'swiper/css';
+import 'swiper/css/navigation';
 
 @Component({
   selector: 'app-about-us',
   imports: [Navbar],
   templateUrl: './about-us.html',
   styleUrl: './about-us.scss',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class AboutUs {
   contentService = inject(ContentService);
 
   content = signal<any>(null);
+  images = [
+    '/images/car-photo.png',
+    '/images/car-photo.png',
+    '/images/car-photo.png',
+    '/images/car-photo.png',
+  ];
 
   ngOnInit(): void {
     this.contentService.getContent('aboutUs').subscribe((data) => {
