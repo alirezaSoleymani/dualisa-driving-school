7a1bab0c12e0e493e922eef9b99227655ecc99aa
alirezaSoleymani <a83.soleimani@gmail.com>
2026-05-05 09:51:33 +0330

    feat: add hero section to avout-us page

--- src/app/pages/about-us/about-us.ts
+++ src/app/pages/about-us/about-us.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { ContentService } from '../../shared/services/content/content-service';
+import { Navbar } from './../../shared/components/navbar/navbar';
+import { Component, inject, signal } from '@angular/core';
 
 @Component({
   selector: 'app-about-us',
-  imports: [],
+  imports: [Navbar],
   templateUrl: './about-us.html',
   styleUrl: './about-us.scss',
 })
 export class AboutUs {
+  contentService = inject(ContentService);
 
+  content = signal<any>(null);
+
+  ngOnInit(): void {
+    this.contentService.getContent('aboutUs').subscribe((data) => {
+      this.content.set(data);
+    });
+  }
 }
