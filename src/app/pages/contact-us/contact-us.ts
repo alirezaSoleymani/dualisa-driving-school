9f68c940864cbaedd7d1a4b7dd7a8f58ebb359e1
alirezaSoleymani <a83.soleimani@gmail.com>
2026-05-07 13:31:29 +0330

    feat: add hero section to contact-us page

--- src/app/pages/contact-us/contact-us.ts
+++ src/app/pages/contact-us/contact-us.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { ContentService } from '../../shared/services/content/content-service';
+import { Navbar } from '../../shared/components/navbar/navbar';
 
 @Component({
   selector: 'app-contact-us',
-  imports: [],
+  imports: [Navbar],
   templateUrl: './contact-us.html',
   styleUrl: './contact-us.scss',
 })
-export class ContactUs {
+export class ContactUs implements OnInit {
+  contentService = inject(ContentService);
+  content = signal<any>(null);
 
+  ngOnInit(): void {
+    this.contentService.getContent('contactUs').subscribe((data) => {
+      this.content.set(data);
+    });
+  }
 }
