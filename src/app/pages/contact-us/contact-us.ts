d5bbea46719f94d1a9f84b160fa628e9e79c9584
alirezaSoleymani <a83.soleimani@gmail.com>
2026-05-09 10:14:28 +0330

    feat: add basic map to contact-us page

--- src/app/pages/contact-us/contact-us.ts
+++ src/app/pages/contact-us/contact-us.ts
@@ -1,16 +1,24 @@
 import { Component, inject, OnInit, signal } from '@angular/core';
 import { ContentService } from '../../shared/services/content/content-service';
 import { Navbar } from '../../shared/components/navbar/navbar';
+import { GoogleMapsModule } from '@angular/google-maps';
 
 @Component({
   selector: 'app-contact-us',
-  imports: [Navbar],
+  imports: [Navbar, GoogleMapsModule],
   templateUrl: './contact-us.html',
   styleUrl: './contact-us.scss',
 })
 export class ContactUs implements OnInit {
   contentService = inject(ContentService);
   content = signal<any>(null);
+
+  center: google.maps.LatLngLiteral = { lat: 46.9505069, lng: 7.4356473 };
+  zoom = 14;
+  markerPosition: google.maps.LatLngLiteral = {
+    lat: 46.9505069,
+    lng: 7.4356473,
+  };
 
   ngOnInit(): void {
     this.contentService.getContent('contactUs').subscribe((data) => {
