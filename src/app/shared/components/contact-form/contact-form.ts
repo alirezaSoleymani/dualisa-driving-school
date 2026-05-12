7acdf7de24346b77019a151df5d70f4ac3969289
alirezaSoleymani <a83.soleimani@gmail.com>
2026-05-12 14:22:55 +0330

    feat: integrate Web3Forms in contact-form component and create config.ts file for static data like access key

--- src/app/shared/components/contact-form/contact-form.ts
+++ src/app/shared/components/contact-form/contact-form.ts
@@ -1,3 +1,5 @@
+import { config } from './../../../../config';
+import { HttpClient } from '@angular/common/http';
 import { ContentService } from '../../services/content/content-service';
 import { Component, inject, input, OnInit, signal } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
@@ -10,6 +12,7 @@ import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 })
 export class ContactForm implements OnInit {
   contentService = inject<any>(ContentService);
+  http = inject(HttpClient);
 
   content = signal<any>(null);
   headline = input('');
@@ -25,6 +28,30 @@ export class ContactForm implements OnInit {
   ngOnInit(): void {
     this.contentService.getContent('contactForm').subscribe((data: any) => {
       this.content.set(data);
+    });
+  }
+
+  onSubmit() {
+    if (this.contactForm.invalid) return;
+
+    const formValue = this.contactForm.value;
+    const payload = new FormData();
+
+    payload.append('access_key', config.web3forms.accessKey);
+    payload.append('name', formValue.name ?? '');
+    payload.append('email', formValue.email ?? '');
+    payload.append('phone', formValue.phoneNumber ?? '');
+    payload.append('message', formValue.message ?? '');
+    payload.append('subject', 'New enquiry from driving school website');
+
+    this.http.post(config.web3forms.endpoint, payload).subscribe({
+      next: () => {
+        console.log('Email sent.');
+        this.contactForm.reset();
+      },
+      error: () => {
+        console.error('Failed to send email');
+      },
     });
   }
 }
---
+++ src/config.ts
@@ -0,0 +1,6 @@
+export const config = {
+  web3forms: {
+    accessKey: 'b3e874cf-59f8-424d-97c4-aae51eb471ce',
+    endpoint: 'https://api.web3forms.com/submit',
+  },
+};
