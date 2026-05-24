25db76a938f51495d108efb65dc7d712732cf5fb
alirezaSoleymani <a83.soleimani@gmail.com>
2026-05-24 23:10:01 +0330

    feat(home): open form modal in continue-cta

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,15 +1,17 @@
+import { ModalService } from './../../shared/services/modal/modal-service';
 import { Component, inject, signal } from '@angular/core';
 import { ContentService } from '../../shared/services/content/content-service';
+import { ContactForm } from '../../shared/components/contact-form/contact-form';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [ContactForm],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
 export class Home {
   contentService = inject(ContentService);
-
+  modalService = inject(ModalService);
   content = signal<any>(null);
 
   ngOnInit(): void {
